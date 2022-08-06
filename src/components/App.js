94501d4d1c2e588290543edcd494c26262b8ea1e
Wen Xiao <wennstyle@gmail.com>
2022-08-06 13:53:39 -0400

    added and imported components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
+//import React, {useEffect, useState} from "react";
 import {Button} from '@mui/material';
+import Header from "./Header";
 
 const App = () => {
+  
+  // const [activities, setActivities] = useState([])
+
+  // useEffect(() => {
+  //   fetch("http://localhost:9292/activities")
+  //     .then((r) => r.json())
+  //     .then((activities) => setActivities(activities));
+  // }, []);
+  
   return (
     <div className="App">
       <h1>"Hello World"</h1>
-      <Button color="primary">This is a button</Button>
+      <Header />
     </div>
   );
 }
