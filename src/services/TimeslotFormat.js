54dab4f9a5c11797b982d2227e4d268b46bb6246
Wen Xiao <wennstyle@gmail.com>
2022-09-02 01:23:20 -0400

    helper function to determine if traveler is taken

--- src/services/TimeslotFormat.js
+++ src/services/TimeslotFormat.js
@@ -2,3 +2,20 @@ export function formatTimeslot(timeslots) {
     return timeslots
       .split(",")
   }
+
+export function formatAvailability(traveler, array) {
+    if(array.includes(traveler.id)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  export function bookingsSummary(booking) {
+    if(booking === null){
+      return 
+    } else {
+      return booking
+    }
+  
+  }  
