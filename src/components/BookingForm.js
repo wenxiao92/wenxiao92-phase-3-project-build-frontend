4214e8a0aeafa9aa881ce72c675ccd8512aacc18
Wen Xiao <wennstyle@gmail.com>
2022-09-04 19:44:48 -0400

    Created text field and button to submit booking

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,26 +1,30 @@
 import React, {useState} from "react";
 import { formatAvailability, proxyState } from "../services/TimeslotFormat"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import CreateBooking from "./CreateBooking";
 
 function BookingForm({allTravelers, unavailableTravelers}) {
-
     const [stateForSubmit, setStateForSubmit] = useState([])
-
+    const [bookingName, setBookingName] = useState("")
+    
+    //variable to pull all traveler ID from array
     const findUnavailableTravelers = unavailableTravelers.map((booking) => {
         return booking.traveler_id
     })
-
+    
+    //sets a proxy status to determine render available vs non available travelers
     const reformatTravelers = allTravelers.map((traveler) => ({
         ...traveler,
         availability: formatAvailability(traveler, findUnavailableTravelers)
     }))
-
+    
+    //a proxy callback function used to determine whether the name is selected
     const handleAddTraveler = (e) => {
-        //proxyState(id, array, cb)
         proxyState(e, stateForSubmit, setStateForSubmit)
     }
-    console.log(stateForSubmit)
-
+    //console.log(stateForSubmit) //see which traveler is selected
+    
     const displayNames = reformatTravelers.map((traveler) => (
         <CreateBooking
             key={traveler.id}
@@ -28,13 +32,46 @@ function BookingForm({allTravelers, unavailableTravelers}) {
             handleAddTraveler={handleAddTraveler}
         />
     ))
+    
+    // Set up JSON to post booking
+    // const newBooking = {
+    //     amount,
+    //     date,
+    //     donor,
+    //     organization_id: organization,
+    //     completed: false,
+    // };
+    
+    // const configObj = {
+    //     method: "POST",
+    //     headers: {
+    //         Accept: "application/json",
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(newOrg),
+    // };
+    
+    const handleCreateActivity = (e) => {
+        e.preventDefault();
 
+        console.log(stateForSubmit, bookingName)
+    }
 
     return(
-        <p>
+        <form onSubmit={handleCreateActivity}>
+            <TextField
+                required
+                id="outlined-required"
+                label="Required"
+                defaultValue="Enter Booking Name"
+                value={bookingName}
+                onChange={(e) => setBookingName(e.target.value)}
+            />
+            <p>
+            <button type="submit">Create Activity</button></p>
             <h1>Available Participants:</h1>
             {displayNames}
-        </p>
+        </form>
     )
 }
 
